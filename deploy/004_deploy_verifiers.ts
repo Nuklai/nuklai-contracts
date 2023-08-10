5c304d3cabeaf76a14147186e27cb8934a52f434
Rafael <rafael-mto@allianceblock.io>
2023-08-10 08:24:08 -0400

    feat: add script to deploy data set verifiers

---
+++ deploy/004_deploy_verifiers.ts
@@ -0,0 +1,33 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { dtAdmin } = await getNamedAccounts();
+
+  const deployedAcceptManuallyVerifier = await deploy(
+    "AcceptManuallyVerifier",
+    {
+      from: dtAdmin,
+    }
+  );
+
+  console.log(
+    "AcceptManuallyVerifier deployed successfully at",
+    deployedAcceptManuallyVerifier.address
+  );
+
+  const deployedAcceptAllVerifier = await deploy("AcceptAllVerifier", {
+    from: dtAdmin,
+  });
+
+  console.log(
+    "AcceptAllVerifier deployed successfully at",
+    deployedAcceptAllVerifier.address
+  );
+};
+
+export default func;
+func.tags = ["DatasetVerifiers"];
