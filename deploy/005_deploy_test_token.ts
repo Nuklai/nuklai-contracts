7a13c6301a2fdcca4476f98b30d69fc2ee8a8324
Rafael <rafael-mto@allianceblock.io>
2023-08-22 23:25:50 -0400

    feat(test): improve test token contract and add deploy script

---
+++ deploy/005_deploy_test_token.ts
@@ -0,0 +1,37 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { TestToken } from "@typechained";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, ethers } = hre;
+  const { deploy } = deployments;
+
+  const { dtAdmin } = await getNamedAccounts();
+
+  const deployedTestToken = await deploy("TestToken", {
+    from: dtAdmin,
+  });
+
+  console.log("TestToken deployed successfully at", deployedTestToken.address);
+
+  const token: TestToken = await ethers.getContractAtWithSignerAddress(
+    "TestToken",
+    deployedTestToken.address,
+    dtAdmin
+  );
+
+  const tokenDecimals = await token.decimals();
+  const amountToMint = ethers.parseUnits("1000000000", tokenDecimals);
+
+  await token.mint(dtAdmin, amountToMint);
+
+  console.log(
+    "TestToken minted successfully to wallet address",
+    dtAdmin,
+    "with",
+    ethers.formatUnits(amountToMint, tokenDecimals)
+  );
+};
+
+export default func;
+func.tags = ["TestToken"];
