0eb1887353dcf606196c69395b1dc54f5c92c7d2
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-11 18:24:39 +0300

    Define and export interfaceIds

---
+++ tests/utils/selectors.ts
@@ -0,0 +1,5 @@
+export const IDatasetNFT_Interface_Id = "0x3d778194";
+export const IERC721_Interface_Id = "0x80ac58cd";
+export const IERC165_Interface_Id = "0x01ffc9a7";
+export const IAccessControl_Interface_Id = "0x7965db0b";
+export const IERC721Metadata_Interface_Id = "0x5b5e139f";
