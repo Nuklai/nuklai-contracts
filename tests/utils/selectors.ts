c2100a5c7788f6a5d9604b1f411046a2948686f8
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-12 15:58:17 +0300

    Define and export additional interface ids

--- tests/utils/selectors.ts
+++ tests/utils/selectors.ts
@@ -3,3 +3,4 @@ export const IERC721_Interface_Id = "0x80ac58cd";
 export const IERC165_Interface_Id = "0x01ffc9a7";
 export const IAccessControl_Interface_Id = "0x7965db0b";
 export const IERC721Metadata_Interface_Id = "0x5b5e139f";
+export const IFragmentNFT_Interface_Id = "0x982616df";
