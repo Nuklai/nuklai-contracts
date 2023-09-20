ea4d62ef359452b2e422eba3f99e30e5365beb46
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-21 00:30:30 +0300

    Update IdatasetNFT intefrace id

--- tests/utils/selectors.ts
+++ tests/utils/selectors.ts
@@ -1,4 +1,4 @@
-export const IDatasetNFT_Interface_Id = "0x3d778194";
+export const IDatasetNFT_Interface_Id = "0x457532c7";
 export const IERC721_Interface_Id = "0x80ac58cd";
 export const IERC165_Interface_Id = "0x01ffc9a7";
 export const IAccessControl_Interface_Id = "0x7965db0b";
