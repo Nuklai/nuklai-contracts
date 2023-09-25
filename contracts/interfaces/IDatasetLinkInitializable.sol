a7999e0e232426e8effa2df567da8cdb000b006c
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-25 21:52:52 +0300

    Add natspec docblock

--- contracts/interfaces/IDatasetLinkInitializable.sol
+++ contracts/interfaces/IDatasetLinkInitializable.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.0;
 
 interface IDatasetLinkInitializable {
+  /**
+   * @notice Intialization function
+   * @dev Sets the address of the DatasetNFT contract and the ID of the Dataset NFT token
+   * @param dataset The address of the DatasetNFT contract
+   * @param datasetId The ID of the Dataset NFT token
+   */
   function initialize(address dataset, uint256 datasetId) external;
 }
