31965a6ab8c50e40df1ca889b14b4aab795c85b0
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-26 11:20:27 +0300

    Improve comments in AcceptAllVerifier

--- contracts/verifier/AcceptAllVerifier.sol
+++ contracts/verifier/AcceptAllVerifier.sol
@@ -13,7 +13,7 @@ contract AcceptAllVerifier is IVerifier {
   /**
    * @notice Propose function
    * @dev Calls the VerifierManager's resolve function with a 'true' acceptance flag.
-   * @param id ID of the fragment
+   * @param id ID of the pending Fragment to be accepted
    */
   function propose(address /*fragmentNFT*/, uint256 id, bytes32 /*tag*/) external {
     VerifierManager(msg.sender).resolve(id, true);
