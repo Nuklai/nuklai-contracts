383b7ddbd6911cc71271db2615e916cda990089f
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-27 21:54:38 +0300

    Add deploy script for ProxyAdmin - fix and correctly enumerate previous deployment scripts

---
+++ deploy/002_deploy_DatasetNFT.ts
@@ -0,0 +1,67 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import {DatasetNFT } from '@typechained';
+import { constants } from '../utils';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, ethers } = hre;
+  const { deploy, fixture } = deployments;
+  const { dtAdmin } = await getNamedAccounts();
+
+  await fixture(['ProxyAdmin', 'FragmentNFT']);
+
+  const proxyAdmin = await ethers.getContract('ProxyAdmin');
+  const proxyAdminAddress = await proxyAdmin.getAddress();
+
+  const fragmentImplementation = await ethers.getContract('FragmentNFT');
+  const fragmentImplementationAddress = await fragmentImplementation.getAddress();
+
+  console.log('ProxyAdmin: ', proxyAdminAddress);
+  console.log('DT admin: ', dtAdmin);
+  
+  const deployedDatasetNFT = await deploy('DatasetNFT', {
+    contract: 'DatasetNFT',
+    from: dtAdmin,
+    log: true,
+    proxy: {
+      owner: proxyAdminAddress,
+      proxyContract: 'MyTransparentUpgradeableProxy',
+      execute: {
+        init: {
+          methodName: 'initialize',
+          args: [dtAdmin],
+        },
+      },
+    }
+  });
+  
+  console.log('DatasetNFT deployed successfully at', deployedDatasetNFT.address);
+
+  const dataset: DatasetNFT = await ethers.getContractAtWithSignerAddress(
+    'DatasetNFT',
+    deployedDatasetNFT.address,
+    dtAdmin
+  );
+
+  const fragmentImplementationSet = await dataset.setFragmentImplementation(
+    fragmentImplementationAddress
+  );
+  await fragmentImplementationSet.wait();
+
+  console.log('FragmentNFT Implementation sucessfully set', fragmentImplementationAddress);
+
+  const proxyAdminSet = await dataset.setProxyAdminAddress(proxyAdminAddress);
+  await proxyAdminSet.wait();
+
+  console.log('ProxyAdmin address sucessfully set', proxyAdminAddress);
+
+  const grantedRole = await dataset.grantRole(constants.SIGNER_ROLE, dtAdmin);
+  await grantedRole.wait();
+
+  console.log('DatasetNFT granted role to', dtAdmin);
+
+  if (process.env.TEST !== 'true') await hre.run('etherscan-verify');
+};
+
+export default func;
+func.tags = ['DatasetNFT'];
---
+++ deploy/005_deploy_dataset_factory.ts
@@ -0,0 +1,46 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { DatasetFactory, DatasetNFT } from '@typechained';
+import { constants } from '../utils';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, ethers } = hre;
+  const { deploy, fixture } = deployments;
+  const { dtAdmin } = await getNamedAccounts();
+
+  //await fixture(['DatasetManagers', 'FragmentNFT', 'ProxyAdmin']);
+
+  const dataset = await ethers.getContract('DatasetNFT');
+  const datasetAddress = await dataset.getAddress();
+
+  console.log('DT admin: ', dtAdmin);
+
+  const deployedDatasetFactory = await deploy('DatasetFactory', {
+    from: dtAdmin,
+  });
+
+  console.log('DatasetFactory deployed successfully at', deployedDatasetFactory.address);
+
+  const subscriptionManager = await ethers.getContract('ERC20SubscriptionManager');
+  const distributionManager = await ethers.getContract('DistributionManager');
+  const verifierManager = await ethers.getContract('VerifierManager');
+
+  const datasetFactory: DatasetFactory = await ethers.getContractAtWithSignerAddress(
+    'DatasetFactory',
+    deployedDatasetFactory.address,
+    dtAdmin
+  );
+
+  const datasetConfigured = await datasetFactory.configure(
+    datasetAddress,
+    await subscriptionManager.getAddress(),
+    await distributionManager.getAddress(),
+    await verifierManager.getAddress()
+  );
+  await datasetConfigured.wait();
+
+  if (process.env.TEST !== 'true') await hre.run('etherscan-verify');
+};
+
+export default func;
+func.tags = ['DatasetFactory'];
