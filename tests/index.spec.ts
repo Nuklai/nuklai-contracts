83d787bf26708715a71b3ca0bf841921e4179aaa
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-27 22:00:23 +0300

    Add index for test suites

---
+++ tests/index.spec.ts
@@ -0,0 +1,13 @@
+import datasetFactory from "./DatasetFactory.spec";
+import datasetNFT from "./DatasetNFT.spec";
+import distributionManager from "./DistributionManager.spec";
+import fragmentNFT from "./FragmentNFT.spec";
+import subscriptionManager from "./SubscriptionManager.spec";
+import testERC20Token from "./TestToken.spec";
+
+datasetFactory();
+datasetNFT();
+distributionManager();
+fragmentNFT();
+subscriptionManager();
+testERC20Token();
