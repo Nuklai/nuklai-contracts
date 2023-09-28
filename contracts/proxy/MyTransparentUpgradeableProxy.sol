6cb833fd9d27715803d587d485b7fd21458e9c7a
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-28 17:31:36 +0300

    Delete MyTUP

--- contracts/proxy/MyTransparentUpgradeableProxy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-
-contract MyTransparentUpgradeableProxy is TransparentUpgradeableProxy {
-  constructor(address _logic, address admin_, bytes memory _data) TransparentUpgradeableProxy(_logic, admin_, _data) {}
-}
