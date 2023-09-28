43c2fc2100ac300cdaa703ac6807b6ef8bf1e4f5
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-09-28 17:34:36 +0300

    Use linter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -64,8 +64,8 @@ const config: HardhatUserConfig = {
 
   dependencyCompiler: {
     paths: [
-      "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol",
-      "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol"
+      '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol',
+      '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol',
     ],
   },
 };
