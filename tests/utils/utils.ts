846ce00889083b96b661a6ea0ad28354083c43ec
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-10-02 23:55:41 +0300

    Add getUuidHash() & getUint256FromBytes32() util functions

--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -1,5 +1,14 @@
-import { solidityPackedKeccak256 } from 'ethers';
+import { solidityPackedKeccak256, toBigInt, keccak256, toUtf8Bytes} from 'ethers';
 
 export const encodeTag = (tag: string): string => {
   return solidityPackedKeccak256(['string'], [tag]);
 };
+
+export const getUuidHash = (uuid: string): string => {
+  return solidityPackedKeccak256(['string'], [uuid]);  // same as keccak256(toUtf8Bytes(uuid));
+
+}; 
+
+export const getUint256FromBytes32 = (hash: string): bigint => {
+  return toBigInt(hash);
+}
