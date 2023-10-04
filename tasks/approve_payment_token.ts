cb2f7c9bac5d36370d7cb88a7e149dc529a6532d
ApostolosMavro <apostolos.mavropoulos@allianceblock.io>
2023-10-04 13:04:26 +0300

    Add custom tasks for payment token approval-revocation

---
+++ tasks/approve_payment_token.ts
@@ -0,0 +1,30 @@
+import { DatasetNFT } from "../typechain-types"
+import { Addressable } from 'ethers';
+import { task } from "hardhat/config";
+
+interface TaskArgs {
+    pk: string;
+    contractAddress: Addressable;
+    tokenAddress: Addressable;
+}
+
+task('approve_payment_token', 'Approves a specific token for subscription fee payments')
+  .addParam('pk', 'Signer private key with ADMIN_ROLE')
+  .addParam('contractAddress', 'Address of the DatasetNFT contract')
+  .addParam('tokenAddress', 'Address of the token to approve')
+  .setAction(async (taskArgs: TaskArgs) => {
+    if (!taskArgs.contractAddress || !taskArgs.tokenAddress) throw new Error('No address provided');
+
+    const wallet = new ethers.Wallet(taskArgs.pk, ethers.provider);
+
+    const dataset = (await ethers.getContractAt(
+      'DatasetNFT',
+      taskArgs.contractAddress,
+      wallet
+    )) as unknown as DatasetNFT;
+
+    console.log('Approving token', taskArgs.tokenAddress);
+    await (await dataset.approveTokenForPayments(taskArgs.tokenAddress)).wait();
+
+    console.log('Token', taskArgs.tokenAddress, 'successfully approved');
+  });
---
+++ tasks/revoke_payment_token.ts
@@ -0,0 +1,30 @@
+import { DatasetNFT } from "../typechain-types"
+import { Addressable } from 'ethers';
+import { task } from "hardhat/config";
+
+interface TaskArgs {
+    pk: string;
+    contractAddress: Addressable;
+    tokenAddress: Addressable;
+}
+
+task('revoke_payment_token', 'Revokes approval for a specific token')
+  .addParam('pk', 'Signer private key with ADMIN_ROLE')
+  .addParam('contractAddress', 'Address of the DatasetNFT contract')
+  .addParam('tokenAddress', 'Address of the token to revoke approval for')
+  .setAction(async (taskArgs: TaskArgs) => {
+    if (!taskArgs.contractAddress || !taskArgs.tokenAddress) throw new Error('No address provided');
+
+    const wallet = new ethers.Wallet(taskArgs.pk, ethers.provider);
+
+    const dataset = (await ethers.getContractAt(
+      'DatasetNFT',
+      taskArgs.contractAddress,
+      wallet
+    )) as unknown as DatasetNFT;
+
+    console.log('Revoking approval for token', taskArgs.tokenAddress);
+    await (await dataset.revokeTokenApproval(taskArgs.tokenAddress)).wait();
+
+    console.log('Token', taskArgs.tokenAddress, 'successfully revoked');
+  });
